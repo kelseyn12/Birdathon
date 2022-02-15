889434a9a000fc91a13a985225c05c4b4b5d9463
kelsey nocek <kelseyn12@hotmail.com>
2022-02-15 12:49:00 -0600

    added a detail page and models page

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Bird(models.Model):
+        name = models.CharField(max_length=100)
+        family = models.CharField(max_length=100)
+        habitat = models.CharField(max_length=100)
+        food = models.CharField(max_length=100)
+        nesting = models.CharField(max_length=100)
+        behavior = models.CharField(max_length=100)
+        description = models.TextField(max_length=250)
+
+       
+        def __str__(self):
+            return self.name
+
+        
+# birds = [
+#     Bird('Northern Flicker', 'Picidae', 'Open Woodlands', 'Insects', 'Cavity', 'Ground Forager', 'Large. brownwood woodpeckers with a gentle expression and handsome black-scalloped plumage'),
+#     Bird('Northern Cardinal', 'Cardinalidae', 'Open Woodlands', 'Seeds', 'Shrub', 'Ground Forager', "A shade of red you cant't take your eyes off of."),
+#     Bird('Golden Eagle', 'Accipitridae', 'Grasslands', 'Mammals', 'Cliff', 'Soaring', "Large and fast nimble raptor. Gold Feathers gleam on the back of it's head"),
+# ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
-
+from .models import Bird
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('<h1>Hello Birds</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
+
+def birds_index(request):
+    birds = Bird.objects.all()
+    return render(request, 'birds/index.html', { 'birds' : birds })
+
+def birds_detail(request, bird_id):
+    bird = Bird.objects.get(id=bird_id)
+    return render(request, 'birds/detail.html', { 'bird' : bird })
